2c856c7b4b9ca7a2b6d04d78fbc1bedd1384690d
tmoynandy <tmoy.nandy@gmail.com>
2019-04-22 14:17:48 +0530

    added signin UI + firebase auth + redirect

--- src/store.js
+++ src/store.js
@@ -63,6 +63,23 @@ export default new Vuex.Store({
                     console.log(error)
                 }
             )
+        },
+        signUserIn({commit}, payload){
+            firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+            .then(
+                user => {
+                    const newUser ={
+                        id : user.uid,
+                        registeredMeetups : []
+                    }
+                    commit('setUser', newUser)
+                }
+            )
+            .catch(
+                error =>{
+                    console.log(error)
+                }
+            )
         }
     },
     getters: {
